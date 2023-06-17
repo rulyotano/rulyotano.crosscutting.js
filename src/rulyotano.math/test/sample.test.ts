7eddd09b7058ad158992dd1f3e49d7fbf63b7913
Raul <rulyotano@gmail.com>
2023-06-18 00:03:11 +0200

    Initial commit (#1)
    * Initial configuration
    
    ---------
    
    Co-authored-by: Raul <rulotano@gmail.com>

---
+++ src/rulyotano.math/test/sample.test.ts
@@ -0,0 +1,14 @@
+import {doSomeStuff} from '../src';
+
+// jest.mock("../defaultMenuItems");
+
+describe('... > sample', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  test('Fake test', () => {
+    doSomeStuff('', '', []);
+    expect(1).toBeTruthy();
+  });
+});
